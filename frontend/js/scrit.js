a3bd62bb1af47b1d53b5a4b3ccf8930c463b9bf3
Geysiane Ramos <85877208+Geysianee@users.noreply.github.com>
2024-01-09 16:35:28 -0300

    Update scrit.js

--- frontend/js/scrit.js
+++ frontend/js/scrit.js
@@ -87,7 +87,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-cf9x.onrender.com")
     websocket.onmessage = processMessage
    
 
